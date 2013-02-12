0259fddfe8c5e5ce3a8eff60665950381c7210f3
Ricardo Rodríguez <rrodrigu3z@gmail.com>
2013-02-12 09:22:56 -0430

    Agregados assets

--- lib/rails_admin_jstree/engine.rb
+++ lib/rails_admin_jstree/engine.rb
@@ -1,4 +1,18 @@
 module RailsAdminJstree
   class Engine < ::Rails::Engine
+    
+    initializer "RailsAdminNestable precompile hook" do |app|
+      app.config.assets.precompile += %w(
+        rails_admin/rails_admin_jstree.js 
+        rails_admin/jquery.jstree.js 
+        rails_admin/jquery.cookie.js 
+        rails_admin/jquery.hotkeys.js 
+        rails_admin_jstree/theme/apple/style.css 
+        rails_admin_jstree/theme/classic/style.css
+        rails_admin_jstree/theme/default/style.css
+        rails_admin_jstree/theme/default-rtl/style.css
+      )
+    end
+    
   end
 end
